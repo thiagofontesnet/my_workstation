43c08db6228e80514dcbade95bc380963886dad4
thiagofontes.net <particular@thiagofontes.net>
2021-12-06 16:31:00 -0300

    adicionado arquivo simples

---
+++ workstation.py
@@ -0,0 +1,18 @@
+import os
+print('Script de instalação iniciado')
+
+#   ultilitários
+os.system('apt install -y qbittorrent')
+
+#   Ferramentas de programação
+os.system('apt install -y git')
+
+#   machines
+os.system('apt install -y virtualbox-qt')
+os.system('apt install -y vagrant')
+os.system('apt install -y ansible')
+os.system('apt install -y docker.io')
+os.system('apt install -y docker-compose')
+
+#   
+os.system('apt install -y anydesk')
